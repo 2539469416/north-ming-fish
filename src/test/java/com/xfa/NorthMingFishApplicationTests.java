e1cb1b025464d2e56038bf70cb11b1ddeeae4220
0xfa <z583985166@gmail.com>
2023-05-03 09:31:51 +0800

    Initial commit

---
+++ src/test/java/com/xfa/NorthMingFishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthMingFishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

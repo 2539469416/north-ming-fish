02e3415825f873af82114712f1526188b70bc35e
北冥有鱼 <z583985166@gmail.com>
2023-05-29 18:24:44 +0800

    股票量化init

--- src/main/java/com/xfa/NorthMingFishApplication.java
+++ src/main/java/com/xfa/NorthMingFishApplication.java
@@ -1,8 +1,13 @@
 package com.xfa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author 北冥有鱼
+ */
 @SpringBootApplication
 public class NorthMingFishApplication {
 
